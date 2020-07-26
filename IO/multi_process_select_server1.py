39b0380ecf4e0b5f0e0f5f2a41cbffe3afd150eb
wangmengyu <m15901428525@163.com>
2020-07-26 17:35:31 +0800

    add .

--- IO/multi_process_select_server1.py
+++ IO/multi_process_select_server1.py
@@ -13,24 +13,27 @@ import socket
 import select
 import multiprocessing
 
+# TODO: 每个worker公平的抢占client 连接
+
 
 class Worker(multiprocessing.Process):
-    def __init__(self, socket):
+    def __init__(self, socket, lock):
         super(Worker, self).__init__()
         self.socket = socket
+        self.lock = lock
         self.rlist = [self.socket, ]
         self.wlist = []
         self.rdata = {}
         self.wdata = {}
 
     def run(self):
-        print "开始socket服务"
-
-        while True:
+        print "{} 开始socket服务".format(self.name)
+        while True:  # 给抢到的socket加上锁
             rl, wl, xl = select.select(self.rlist, self.wlist, [])
             for sock in rl:  # 对返回值rl判断是否有socket进来，如果有连接进来的话sock激活
                 if sock == self.socket:  # 代表有新的链接进来了
-                    print "有新连接进来了 ", rl
+
+                    print "{} 有新连接进来了 ".format(self.name)
                     conn, addr = sock.accept()
                     self.rlist.append(conn)  # 把新的客户端链接放到监听列表中，继续托管给select去监听
                 else:  # 由于客户端连接进来时socket接收客户端连接请求，将客户端连接加入到了监听列表中(rlist)，客户端发送消息的时候这个连接将触发
@@ -38,11 +41,11 @@ class Worker(multiprocessing.Process):
                         print "客户端发送数据了>>", type(sock)
                         data = sock.recv(1024)  # 接收客户端请求
                         if not data:
-                            print "什么都没收到，关闭客户端连接..."
+                            print "{} 什么都没收到，关闭客户端连接...".format(self.name)
                             sock.close()
                             self.rlist.remove(sock)
                             continue
-                        print "received data [{0}] from client {1}".format(data.decode(), sock)
+                        print "{} received data [{}] from client {}".format(self.name, data.decode(), sock)
 
                         self.rdata[sock] = data.decode()  # 将接受到的客户端信息保存下来
                         self.wdata[sock] = data.upper()  # 加工成返回消息, 这里相当于处理客户端请求
@@ -52,7 +55,7 @@ class Worker(multiprocessing.Process):
                         self.rlist.remove(sock)
 
             for sock in wl:  # 当有可写事件发生的时候，代表可以给客户端返回数据，
-                print "当有可写事件发生的时候，", wl
+                print "{} 当有可写事件发生的时候，".format(self.name)
                 sock.send(self.wdata[sock])  # 将要返回的数据放到sock中，
                 # 发送完从可写列表中移除 socket，避免重复发送
                 self.wlist.remove(sock)
@@ -62,12 +65,15 @@ class Worker(multiprocessing.Process):
 def start_server():
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("127.0.0.1", 8800))  # 绑定端口号和地址
-
     server.listen(5)  # 最大接收5 个socket请求
     server.setblocking(False)  # 设置为非阻塞IO
-    for _ in range(multiprocessing.cpu_count()):
-        p = Worker(server)
-        p.start()
+    lock = multiprocessing.Lock()
+    worker_list = [Worker(server, lock) for _ in range(multiprocessing.cpu_count())]
+
+    for worker in worker_list:
+        worker.start()
+    for worker in worker_list:
+        worker.join()
 
 
 if __name__ == '__main__':
