be61a469b304e7dae58940fea3c3c362f3c68660
wangmengyu <wangmengyu@yupiao.com>
2020-08-11 21:53:11 +0800

    update

--- chapter1/pyck14.py
+++ chapter1/pyck14.py
@@ -6,12 +6,13 @@ import random
 查找最大或最小的N个元素
 """
 
+
 def main():
-	print dir(heapq)
-	l = range(10, 20)
-	random.shuffl(l)
-	print heapq.nlargest(3, l)
+    print dir(heapq)
+    l = range(10, 20)
+    random.shuffle(l)
+    print heapq.nlargest(3, l)
 
 if __name__ == '__main__':
-	main()
+    main()
 
