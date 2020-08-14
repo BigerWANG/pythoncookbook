7bd928e24f77d067616ca61e06e88af6a5f0c969
王孟宇 <wangmengyu04@maoyan.com>
2020-08-14 11:19:53 +0800

    add

--- DesignPattern/singleton.py
+++ DesignPattern/singleton.py
@@ -11,33 +11,74 @@
 
 一般情况下，设计以简单工单模式或者工厂方法模式开始，当你发现设计需要更大的灵活性时，则向更复杂的设计模式演化
 
+应用场景：
+单例模式具有整体性，统一性的优势，适用于以下场景：
+1.资源管理的场景
+2.难以同步的场景
+3.涉及共享的场景
+4.有关认证的场景，设置一个统一认证的类，只需认证一次，随处调用即可
 """
+
+from threading import Lock, Thread
+
+import time
+
+
+def synchronized(func):
+    # 线程安全的装饰器
+    def lock_func(*args, **kwargs):
+        with Lock():
+            return func(*args, **kwargs)
+    return lock_func
 
 
 class Singleton(object):
+    # 重写new方法
+    # @synchronized
+    def __init__(self):
+        time.sleep(1)
+
     def __new__(cls, *args, **kwargs):
-        """重写new方法，new是生成实例之前被调用的方法"""
-        if not hasattr(cls, "_instance"):  # 先看是不是有new方法
-            # 如果没有new方法，则调用父类的new方法去创建实例
+        # 判断实例中是否有instance字段
+        if not hasattr(cls, "_instance"):
             cls._instance = super(Singleton, cls).__new__(cls)
         return cls._instance
 
 
-class MySingleton(Singleton):
-    def __init__(self, a):
-        self.a = a
+class Singleton1(object):
+    _instance_lock = Lock()
+
+    def __init__(self, *args, **kwargs):
+        time.sleep(1)
+
+    @classmethod
+    def instance(cls, *args, **kwargs):
+        with Singleton1._instance_lock:
+            if not hasattr(Singleton1, "_instance"):
+                Singleton1._instance = Singleton1(*args, **kwargs)
+            return Singleton1._instance
 
 
-class MySingleton1(Singleton):
-    def __init__(self, b):
-        self.b = b
+class MyClass(Singleton):
+    pass
 
 
-a = MySingleton("a")
-b = MySingleton("b")  # b 会指向a 因为使用了单例模式
+def task(arg):
+    obj = MyClass()
+    print obj
 
 
-print a.a
-print b.a
+if __name__ == "__main__":
+    # a = Singleton(3)
+    # print("a单例! id为 %s" % id(a))
+    # b = Singleton(4)
+    # print("b单例! id为 %s" % id(b))
+    # str = "hello world !"
+    # c = Singleton(str)
+    # print("c单例! id为 %s" % id(c))
+    # print("ok!")
+    num = 0
+    l = [Thread(target=task, args=(i,)) for i in range(10)]
+    for i in l:
+        i.start()
 
-print a is b
